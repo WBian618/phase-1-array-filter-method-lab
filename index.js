59239a4d53bdd0c132591c53d4b7d7588f611574
webster <wbian618@gmail.com>
2022-12-07 19:32:11 -0500

    commit tree e63a71dc73785d75751a8809d66972d3ef0511db

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+let findMatching = (drivers, driverName) => {
+    for (const driver of drivers)    {
+        if (driver === driverName ) {
+            return driverName
+        }
+
+    }
+}
+
+// function fuzzyMatch(source, testString) {
+//     return source.filter(
+//       (possibleMatch) =>
+//         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     );
+//   }
+
+function fuzzyMatch(source, test) {
+    let matching = source.filter(record => record.toLowerCase().startsWith(test.toLowerCase()))
+    return matching
+}
